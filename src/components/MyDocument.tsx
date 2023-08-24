899102b30ebc578e75c4bd5f53d79ecb359edc44
danjudex <dm@orcatm.com>
2023-08-24 11:02:50 +0000

    fix config

--- src/components/MyDocument.tsx
+++ src/components/MyDocument.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { Document, Page, View, Text } from "@react-pdf/renderer";
 
-export const MyDocument: React.FC = () => (
+export const MyDocument = () => (
   <Document>
     <Page>
       <View>
